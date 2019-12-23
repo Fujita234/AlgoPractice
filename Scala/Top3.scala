916b8123b8eacfd91baf28c2aec2a03c8e643724
Fujita234 <exia.hinata0622@gmail.com>
2019-12-23 17:54:53 +0900

    ScalaでTop3でアルゴリズムを作りました

--- Scala/Top3.scala
+++ Scala/Top3.scala
@@ -1,3 +1,14 @@
-Object Top3 {
-  println("Hello World");
+object Top3 extends App{
+    val list1: List[Int] = List(25, 36, 4, 55, 71, 18, 0, 71, 89, 65);
+    top3(list);
+
+    def top3(list: List[Int]) = {
+        var editedList: List[Int] = list;
+        var maxValue = 0;
+        for (i <- 0 until 3) {
+            maxValue = editedList.max;
+            editedList = editedList.diff(List(maxValue));
+            println(maxValue);
+        };
+    };
 }
