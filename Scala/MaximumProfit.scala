f0d7472448d119926cdd8e9f86b3f8069f0bb08a
Fujita234 <exia.hinata0622@gmail.com>
2020-01-02 21:38:27 +0900

    scalaでmaximumProfitを書きました

---
+++ Scala/MaximumProfit.scala
@@ -0,0 +1,24 @@
+object MaximumProfit extends App{
+
+    maximumProfit(List(6, 5, 3, 1, 3, 4, 3));
+    maximumProfit(List(3, 4, 3, 2));
+    
+    def maximumProfit(list: List[Int]) = {
+        var max = 0;
+        var min = 99999;
+        var i = 0;
+        for (i <- 0 to list.length - 1) {
+            if (max < list.apply(i)) {
+                max = list.apply(i);
+            } 
+            if (min >= list.apply(i)) {
+                min = list.apply(i);
+                max = 0;
+            }
+        }
+        if (max < list.apply(i)) {
+            max = list.apply(i);
+        }
+        println(max - min);
+    }
+}
