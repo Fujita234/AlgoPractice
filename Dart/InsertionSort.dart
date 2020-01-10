1ecfef873477ff655105fa922d8b60624a331120
Fujita234 <exia.hinata0622@gmail.com>
2020-01-10 23:22:51 +0900

    dartで挿入ソートを書きました

---
+++ Dart/InsertionSort.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+  /**
+   * 挿入ソート
+   */
+  List<int> insertionSort(List<int> list) {
+    for (var i = 0; i < list.length; i++) {
+      var beSortedValue = list[i];
+      var j = i - 1;
+      while(j >= 0 && list[j] > beSortedValue) {
+        list[j + 1] = list[j];
+        list[j] = beSortedValue;
+        j--;
+      }
+    }
+    return list;
+  }
+  
+  print(insertionSort([5, 2, 4, 6, 1, 3]));
+}
