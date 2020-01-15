43ce4068057d00ec18f51f49e615a0250efbc55f
Fujita234 <exia.hinata0622@gmail.com>
2020-01-15 18:09:10 +0900

    javaでバブルソートを実装

--- Java/BubbleSort.java
+++ Java/BubbleSort.java
@@ -2,20 +2,33 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
-        print(insertionSort([5, 2, 4, 6, 1, 3]));
         
-        /**
-         * バブルソートの実装
-         */
-        public List<Integer> bubbleSort(List<Integer> list) {
-            boolean flg = true;
-            if (list[i - 1] > list[i]) {
-                
-            }
-            while(flg) {
-                for ()
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(2);
+        list.add(4);
+        list.add(6);
+        list.add(1);
+        list.add(3);
+        
+        System.out.println(bubbleSort(list));
+    }
+    
+    /**
+     * バブルソートをするメソッド
+     * @param list リスト
+     */
+    public static List<Integer> bubbleSort(List<Integer> list) {
+        int n = list.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j >= i + 1; j--) {
+                if (list.get(j) < list.get(j - 1)) {
+                    int beSortedValue = list.get(j);
+                    list.set(j, list.get(j - 1));
+                    list.set(j - 1, beSortedValue);
+                }
             }
         }
+        return list;
     }
 }
