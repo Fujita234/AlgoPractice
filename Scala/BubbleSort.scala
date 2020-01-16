de083456f351c6d7e0be8bfb28539617a76152db
Fujita234 <exia.hinata0622@gmail.com>
2020-01-16 18:40:17 +0900

    scalaでバブルソートを実装

---
+++ Scala/BubbleSort.scala
@@ -0,0 +1,17 @@
+import scala.collection.mutable.ListBuffer
+
+object BubbleSort extends App{
+    // Your code here!
+    def bubbleSort(list: ListBuffer[Int]):ListBuffer[Int] = {
+        var n = list.length;
+        for (i <- 0 until n; j <- n - 1 to i + 1 by -1) {
+            if (list.apply(j) < list.apply(j - 1)) {
+                var beSortedValue = list.apply(j);
+                list.update(j, list.apply(j - 1));
+                list.update(j - 1, beSortedValue);
+            }
+        }
+        return list;
+    }
+    println(bubbleSort(ListBuffer(5, 2, 4, 6, 1, 3)))
+}
