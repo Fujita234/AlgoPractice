8aee9c042110170bf2e3fe7c5a6e56bbecdac88f
Fujita234 <exia.hinata0622@gmail.com>
2020-01-22 17:58:56 +0900

    tsで選択ソート実装

---
+++ JavaScript/TypeScript/SelectionSort.ts
@@ -0,0 +1,23 @@
+/**
+ * 選択ソート
+ * @param list リスト
+ */
+const SelectionSort = (list: number[]) => {
+  let i = 0;
+  let j = 0;
+  const n = list.length;
+  for (i = 0; i < n; i++) {
+      let minj = i;
+      for (j = minj; j < n; j++) {
+          if (list[minj] > list[j]) {
+              let beSortedValue = list[j];
+              list[j] = list[minj];
+              list[minj] = beSortedValue;
+          }
+      }
+  }
+  return list;
+}
+
+
+console.log(selectionSort([5, 2, 4, 3, 6, 1]));
