496a4dfd56528422501a65a2a789e83159f5e0a2
Fujita234 <exia.hinata0622@gmail.com>
2020-01-22 18:06:49 +0900

    選択ソートをdartで実装

---
+++ Dart/SelectionSort.dart
@@ -0,0 +1,22 @@
+void main() {
+  
+  /**
+   * 選択ソート
+   * @param list リスト
+   */
+  List<int> selectionSort(List<int> list) {
+    final n = list.length;
+    for (var i = 0; i < n; i++) {
+      var minj = i;
+      for (var j = minj; j < n; j++) {
+        if (list[minj] > list[j]) {
+          var beSortedValue = list[j];
+          list[j] = list[minj];
+          list[minj] = beSortedValue;
+        }
+      }
+    }
+    return list;
+  }
+  print(selectionSort([4, 2, 5, 6, 1, 3]));
+}
