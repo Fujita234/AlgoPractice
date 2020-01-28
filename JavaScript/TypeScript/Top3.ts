1e8f43355cd3f8595441244d4ef49fe33d7493fe
Fujita234 <exia.hinata0622@gmail.com>
2020-01-28 23:40:46 +0900

    functionになっていたのでconstに修正

--- JavaScript/TypeScript/Top3.ts
+++ JavaScript/TypeScript/Top3.ts
@@ -1,7 +1,7 @@
 const list: number[] = [25, 36, 4, 55, 71, 18, 0, 71, 89, 65];
 
-aaa(list);
-function aaa(list: number[]) {
+top3(list);
+const top3 = (list: number[]) => {
     let bbb: number[] =
         list.sort((num1, num2) => num2 - num1);
     for (let i = 0; i < 3; i++) {
