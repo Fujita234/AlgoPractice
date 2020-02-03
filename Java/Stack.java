6b11da20d588ab8ef27ba1ad57f342ca9b26797e
Fujita234 <exia.hinata0622@gmail.com>
2020-02-03 17:19:13 +0900

    Stackをjavaで実装

---
+++ Java/Stack.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("XXXXXXXX");
+        
+    }
+    
+    public static List<Integer> stack() {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        int pop1 = 0;
+        int pop2 = 0;
+        if (str.epuals("+")) {
+            pop1 = pop();
+            pop2 = pop();
+            push(pop1 + pop2);
+        } else if (str.epuals("-")) {
+            pop2 = pop();
+            pop1 = pop();
+            push(pop1 - pop2);
+        } else if (str.epuals("*")) {
+            pop1 = pop();
+            pop2 = pop();
+            push(pop1 * pop2);
+        } else {
+            
+        }
+    }
+    
+    public static List<Integer> push(List<Integer> list, Integer addValue) {
+        list.add(addValue);
+        return list;
+    }
+    
+    /**
+     * 
+     */
+    public static List<Integer> pop(List<Integer> list) {
+        Integer n = list.length - 1;
+        Integer popedValue = list.get(n);
+        list.remove(n);
+        return popedValue;
+    }
+}
