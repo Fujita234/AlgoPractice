b27723cb1f13642b423e7be3ff4cb0cc6279f808
Fujita234 <exia.hinata0622@gmail.com>
2020-02-20 23:14:06 +0900

    jsでstackを実装

---
+++ JavaScript/JavaScript/stack.js
@@ -0,0 +1,33 @@
+/**
+ * stackを実装
+ * @param {リスト} lists 
+ */
+function stack(lists) {
+  let stackList = [];
+  for (let i = 0; i < lists.length; i++) {
+      let num = Number(lists[i]);
+      if (isNaN(num)) {
+          if (lists[i] === "+") {
+              pop1 = stackList.pop();
+              pop2 = stackList.pop();
+              stackList.push(pop1 + pop2);
+          } else if (lists[i] === "-") {
+              pop1 = stackList.pop();
+              pop2 = stackList.pop();
+              stackList.push(pop1 - pop2);
+          } else if (lists[i] === "*") {
+              pop1 = stackList.pop();
+              pop2 = stackList.pop();
+              stackList.push(pop1 * pop2);
+          } else {
+              console.log(e);
+          }
+      } else {
+          stackList.push(num);
+      }
+  }
+  return stackList;
+}
+
+
+console.log(stack(["1","2","+","3","4","-","*"]));
